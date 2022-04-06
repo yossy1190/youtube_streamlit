ea5a400c4a33fc91342a796d880220ece9495e86
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-04-06 18:06:03 +0900

    coding complete at VScode

--- detect_update.py
+++ detect_update.py
@@ -1,6 +1,9 @@
+from encodings import utf_8
 from bs4 import BeautifulSoup
 import requests
 import re
+import line
+# import utf_8
 
 def detect_update():
     url="https://www.statusparty.jp/schedule/tokyo/ginza/13019/"
@@ -18,10 +21,13 @@ def detect_update():
 
     if new_elem==old_elem:
         print('更新はありません')
+        line.main("更新はありません")
         return True
     else:
         with open("old_elem.txt","w") as f:
             old_elem=f.write(new_elem)
         print('更新されました')
+        line.main("更新されました")
         return False
+    
 detect_update()
--- line.py
+++ line.py
@@ -7,16 +7,12 @@ info=json.load(file)
 
 CHANNEL_ACCESS_TOKEN=info['CHANNEL_ACCESS_TOKEN']
 # channel access tokenを読み込み
-
 line_bot_api=LineBotApi(CHANNEL_ACCESS_TOKEN)
 # linebotapiをインスタンス化
 
-def main():
+def main(text):
     USER_ID=info['USER_ID']
     # 誰に送るか、をここで明記したよ。
-    messages=TextSendMessage(text='testです')
+    messages=TextSendMessage(text=text)
     line_bot_api.push_message(USER_ID,messages)
     # 引数に、「誰に」「なんのメッセージ」を記載
-
-if __name__ =="__main__":
-    main()

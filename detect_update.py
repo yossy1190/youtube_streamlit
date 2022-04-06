4b0bd767c2797476c674d679c11f92975dd43be3
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-04-06 17:52:53 +0900

    1st commit

---
+++ detect_update.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def detect_update():
+    url="https://www.statusparty.jp/schedule/tokyo/ginza/13019/"
+    res=requests.get(url)
+    soup=BeautifulSoup(res.text,"html.parser")
+    new_elems=soup.select(".type_acceptable")
+    new_elem=str(new_elems[1])
+    print(new_elem)
+
+    try:
+        with open("old_elem.txt") as f:
+            old_elem=f.read()
+    except:
+        old_elem=""
+
+    if new_elem==old_elem:
+        print('更新はありません')
+        return True
+    else:
+        with open("old_elem.txt","w") as f:
+            old_elem=f.write(new_elem)
+        print('更新されました')
+        return False
+detect_update()
